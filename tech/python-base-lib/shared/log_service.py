5481e75b46a94f6067b3a9627395d5d3e6a8cea7
lonelyhentai <lonely_hentai@hotmail.com>
2018-09-10 11:24:19 +0800

    python-base-lib adjust dirs

---
+++ tech/python-base-lib/shared/log_service.py
@@ -0,0 +1,22 @@
+from injector import inject
+import logging
+from shared.path_service import PathService
+import sys
+
+
+class LogService:
+    @inject
+    def __init__(self, path_service: PathService):
+        self.path_service: PathService = path_service
+        file_handle = logging.FileHandler(
+            path_service.get_log('default.log'), mode='a+')
+        console_handle = logging.StreamHandler(stream=sys.stdout)
+        logger = logging.getLogger("default")
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handle.setFormatter(formatter)
+        console_handle.setFormatter(formatter)
+        logger.addHandler(file_handle)
+        logger.addHandler(console_handle)
+        logger.info('[LogService]:LogService initialized.')
+        self.logger: logging.Logger = logger
